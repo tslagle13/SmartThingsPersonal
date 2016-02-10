e1395e476afde21e6fca37a57795c7997398ac5a
Tim Slagle <tslagle13@gmail.com>
2016-02-10 02:15:27 -0800

    initial commit

---
+++ smartapps/tslagle13/bloomsky-scheduler.src/bloomsky-scheduler.groovy
@@ -0,0 +1,53 @@
+/**
+ *  Bloomsky Scheduler
+ *
+ *  Copyright 2016 Tim Slagle
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Bloomsky Scheduler",
+    namespace: "tslagle13",
+    author: "Tim Slagle",
+    description: "Schedules Bloomsky Device",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Select your bloomsky") {
+        input "bloomsky", "device.Bloomsky", required: true
+    
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	runEvery5Minutes(refreshBloomsky)
+}
+
+def refreshBloomsky() {
+	bloomsky.refresh()
+}
