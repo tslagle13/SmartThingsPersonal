fe1548ec0d2e220e561dc7052e559d189b25d503
Tim Slagle <tslagle13@gmail.com>
2016-02-19 17:47:52 -0800

    Re include lux

--- devicetypes/tslagle13/bloomsky.src/bloomsky.groovy
+++ devicetypes/tslagle13/bloomsky.src/bloomsky.groovy
@@ -3,6 +3,8 @@
  *
  *  This DTH requires the BloomSky (Connect) app (https://github.com/tslagle13/SmartThingsPersonal/blob/master/smartapps/tslagle13/bloomsky-connect.src/bloomsky-connect.groovy)
  *
+ *  Version: 1.0.4 - reincluded lx after bloomsky api change
+ *
  *	Version: 1.0.3 - Removed Lux value. Dynamic Lux value will no longer be supported in this device type.
  * 	
  *  Version: 1.0.2 - Added new tile to display last time the data was retrieved - lastUpdated  @thrash99er
@@ -58,10 +60,10 @@ metadata {
 		carouselTile("cameraDetails", "device.image", width: 4, height: 4) { }
         standardTile("refresh", "device.weather", decoration: "flat", width: 2, height: 2) {
 			state "default", label: "", action: "refresh", icon:"st.secondary.refresh"
-		}/*
+		}
         valueTile("light", "device.illuminance", decoration: "flat", width: 2, height: 2) {
 			state "default", label:'${currentValue} Lux'
-		}*/
+		}
         valueTile("rain", "device.rain", decoration: "flat", width: 2, height: 2) {
 			state "default", label:'${currentValue}'
 		}
@@ -103,7 +105,7 @@ metadata {
 				]
 		}
         main(["temperature"])
-		details(["cameraDetails", "temperature", "uv", "humidity", "pressure", "rain", "night", "battery", "lastUpdated", "refresh"])}	
+		details(["cameraDetails", "temperature", "uv", "humidity", "pressure","light",  "rain", "night", "battery", "lastUpdated", "refresh"])}	
 }    
 
 def poll() {
@@ -171,10 +173,10 @@ def callAPI() {
                             log.debug "Humidity:" + humidity
                         }
                     }    
-                }/*
+                }
                 if (individualBloomSky.Data.Luminance) {
                     def L =  individualBloomSky.Data.Luminance.toString()
-                    def luminance = ((L.replaceAll("\\[", "").replaceAll("\\]","")) as int).intdiv(3600)
+                    def luminance = L.replaceAll("\\[", "").replaceAll("\\]","")
                     if (luminance != state.currentLuminance) {
                         sendEvent(name: "illuminance", value: luminance)
                         state.currentLuminance = luminance
@@ -183,7 +185,7 @@ def callAPI() {
                             log.debug "Luminance:" + luminance
                         }
 					}
-                }*/
+                }
                 if (individualBloomSky.Data.Pressure) {
                     def P =  individualBloomSky.Data.Pressure.toString()
                     def pressure = (P.replaceAll("\\[", "").replaceAll("\\]","").take(4))
