15ecb7512bc4f47ebc8f36ac29d678a413b57b56
Tim Slagle <tslagle13@gmail.com>
2016-02-21 17:55:44 -0800

    - Version: 1.0  - Perfomance improvements. The DTH now updates almsot twice as fast!  - Updated the "Rain" tile to a "Water Sensor" tile. You can use bloomsky in any water detection app now!  - Removed the "It's Night Time" tile. It was sort of meaningless.

--- devicetypes/tslagle13/bloomsky.src/bloomsky.groovy
+++ devicetypes/tslagle13/bloomsky.src/bloomsky.groovy
@@ -3,6 +3,10 @@
  *
  *  This DTH requires the BloomSky (Connect) app (https://github.com/tslagle13/SmartThingsPersonal/blob/master/smartapps/tslagle13/bloomsky-connect.src/bloomsky-connect.groovy)
  *
+ *  Version: 1.0 - Perfomance improvements. The DTH now updates almsot twice as fast!
+ *				 - Updated the "Rain" tile to a "Water Sensor" tile. You can use bloomsky in any water detection app now!
+ *				 - Removed the "It's Night Time" tile. It was sort of meaningless. 
+ * 
  *  Version: 1.0.7 - Fixed trailing decimcals for android.
  *
  *	Version: 1.0.6 - fixed code halting issue with temp conversion introduced by 1.0.5 and fixed last updated display issue
@@ -10,24 +14,6 @@
  *  Version: 1.0.5 - fixed celcius support, i had removed by accident - @thrash99er
  *
  *  Version: 1.0.4 - reincluded lx after bloomsky api change
- *
- *	Version: 1.0.3 - Removed Lux value. Dynamic Lux value will no longer be supported in this device type.
- * 	
- *  Version: 1.0.2 - Added new tile to display last time the data was retrieved - lastUpdated  @thrash99er
- * 
- *	Version: 1.0.1 - Fixed issue where DTH would not update at night. 
- *
- *	Version: 1.0 - Feature!: This device now has a device manager.
- *                      - Uninstall the current bloomsky device from ST. Copy this code overtop the current DTH
- *						  and install the connect app. The Connect app will create a new device for you.
- *				 - Logging level taken from parent app
- *				 - API Key transmitted from parent app in secure manner
- *				 - Only updates DTH when current value changes. Keeps history clean and removes needless send events 
- *
- *	Version: 0.5 - Celcius support thanks to @terafin
- *				 - Specific device refresh thanks to @thrash99er
- *               
- * 
  *
  *  Copyright 2016 Tim Slagle
  *
@@ -41,6 +27,8 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+ 
+
 metadata {
 	definition (name: "Bloomsky", namespace: "tslagle13", author: "Tim Slagle") {
 		capability "Battery"
@@ -50,6 +38,7 @@ metadata {
 		capability "Sensor"
 		capability "Temperature Measurement"
 		capability "Ultraviolet Index"
+        capability "Water Sensor"
         
         attribute "day", "string"
         attribute "pressure", "number"
@@ -70,8 +59,9 @@ metadata {
         valueTile("light", "device.illuminance", decoration: "flat", width: 2, height: 2) {
 			state "default", label:'${currentValue} Lux'
 		}
-        valueTile("rain", "device.rain", decoration: "flat", width: 2, height: 2) {
-			state "default", label:'${currentValue}'
+        standardTile("water", "device.water", width: 2, height: 2) {
+			state "dry", icon:"st.alarm.water.dry", backgroundColor:"#ffffff"
+			state "wet", icon:"st.alarm.water.wet", backgroundColor:"#53a7c0"
 		}
         valueTile("uv", "device.ultravioletIndex", width: 2, height: 2) {
 			state "default", label:'${currentValue} UV',
@@ -92,9 +82,6 @@ metadata {
         valueTile("battery", "device.battery", decoration: "flat", width: 2, height: 2) {
 			state "default", label:'${currentValue} mV Battery'
 		}
-        valueTile("night", "device.day", decoration: "flat", width: 2, height: 2) {
-			state "default", label:'${currentValue}'
-		}
          valueTile("lastUpdated", "device.lastUpdated", decoration: "flat", width: 2, height: 2) {
 			state "default", label:'${currentValue}'
 		}
@@ -111,7 +98,7 @@ metadata {
 				]
 		}
         main(["temperature"])
-		details(["cameraDetails", "temperature", "uv", "humidity", "pressure","light",  "rain", "night", "battery", "lastUpdated", "refresh"])}	
+		details(["cameraDetails", "temperature", "uv",  "water", "humidity", "pressure","light", "battery", "lastUpdated", "refresh"])}	
 }    
 
 def poll() {
@@ -128,9 +115,11 @@ def getTemperature(value) {
     return convertTemperatureIfNeeded(value.toFloat(), "F", 4)
 }
 
+//call Bloomsky API and update device
 def callAPI() {
 	log.debug "Refreshing Bloomsky Device - ${device.label}"
 	def logging = parent.loggingOn()
+    //building the http request for the bloomsky API
     def pollParams = [
         uri: "http://thirdpartyapi.appspot.com",
         path: "/api/skydata/",
@@ -139,8 +128,9 @@ def callAPI() {
 		]
         try {
             httpGet(pollParams) { resp ->
+            	
                 // Get the Device Info of the Correct Bloom Sky
-                def individualBloomSky
+                def individualBloomSky = resp.getData().findAll{ it.DeviceID == device.deviceNetworkId }
                 // If you don't have Device ID specific get the first bloom sky only
                 if(device.deviceNetworkId) {
                     individualBloomSky = resp.getData().findAll{ it.DeviceID == device.deviceNetworkId }
@@ -148,155 +138,64 @@ def callAPI() {
                 else {
                     individualBloomSky = resp.data[0]
                 }
-                
-              
-                // def timeString = new Date(ts + location.timeZone.rawOffset ).format("yyyy-MM-dd HH:mm")
-                // log.debug "TSS " + timeString
-				
-                if (logging) {
-                    log.debug ("This BloomSky: " + individualBloomSky)
-                }
-               
-				//if statements crawl through json to send events for each data point
-                if (individualBloomSky.Data.Temperature) {
-                    def T =  individualBloomSky.Data.Temperature.toString()
-                    def temp = T.replaceAll("\\[", "").replaceAll("\\]","")
-                    temp = getTemperature(temp).toDouble().trunc(1)
-                    //if (temp != state.currentTemp) {
-                        sendEvent(name: "temperature", value: temp, unit: "F")
-                        state.currentTemp = temp  
-                    //}
-                    if (logging) {
-                        log.debug "Temp:" + temp
-                    }
-                }
-                if (individualBloomSky.Data.Humidity) {
-                    def H =  individualBloomSky.Data.Humidity.toString()
-                    def humidity = H.replaceAll("\\[", "").replaceAll("\\]","")
-                    if (humidity != state.currentHumidity) {
-                        sendEvent(name: "humidity", value: humidity, unit: "%")
-                        state.currentHumidity = humidity
-                    } 
-                    if (logging) {
-                            log.debug "Humidity:" + humidity
-                    }
-                }
-                if (individualBloomSky.Data.Luminance) {
-                    def L =  individualBloomSky.Data.Luminance.toString()
-                    def luminance = L.replaceAll("\\[", "").replaceAll("\\]","")
-                    if (luminance != state.currentLuminance) {
-                        sendEvent(name: "illuminance", value: luminance)
-                        state.currentLuminance = luminance
-                    }
-                    if (logging) {
-                            log.debug "Luminance:" + luminance
-                    }
-                }
-                if (individualBloomSky.Data.Pressure) {
-                    def P =  individualBloomSky.Data.Pressure.toString()
-                    def pressure = P.replaceAll("\\[", "").replaceAll("\\]","").toDouble().trunc(1)
-                    if (pressure != state.currentPressure) {
-                        sendEvent(name: "pressure", value: pressure, unit: "inHg")
-                        state.currentPressure = pressure
-                    }
-                    if (logging) {
-                            log.debug "Pressure:" + pressure
-                    }
-                }
-                if (individualBloomSky.Data.UVIndex) {
-                    def U =  individualBloomSky.Data.UVIndex.toString()
-                    def uvIndex = U.replaceAll("\\[", "").replaceAll("\\]","")
-                    if (uvIndex != state.currentIlluminance) {
-                        sendEvent(name: "ultravioletIndex", value: uvIndex)
-                        state.currentIlluminance = uvIndex
-                    }
-                    if (logging) {
-                            log.debug "uvIndex:" + uvIndex
-                    }
-                }
-                if (individualBloomSky.Data.Voltage) {
-                    def V =  individualBloomSky.Data.Voltage.toString()
-                    def voltage = V.replaceAll("\\[", "").replaceAll("\\]","")
-                    if (voltage != state.currentBattery) {
-                        sendEvent(name: "battery", value: voltage)
-                        state.currentBattery = voltage
-                    }
-                    if (logging) {
-                            log.debug "voltage:" + voltage
-                    }
-                }
-                if (individualBloomSky.Data.ImageURL) {
-                    def I =  individualBloomSky.Data.ImageURL.toString()
-                    def image = I.replaceAll("\\[", "").replaceAll("\\]","").toString()
-                        httpGet(image) { it -> 
-                            storeImage(getPictureName(), it.data)
-                    }
-                    if (logging) {
-                            log.debug "image:" + image
-                    }
-                }
-                
-                def newTS =  individualBloomSky.Data.TS.toString()
-                def lastUpdated = Long.parseLong(((newTS.replaceAll("\\[", "").replaceAll("\\]",""))))
-                def lastUpdateTick = (lastUpdated * 1000L) + location.timeZone.rawOffset
-                def finalUpdated = new java.text.SimpleDateFormat("MMM dd HH:mm").format(lastUpdateTick)
-               
-                sendEvent(name: "lastUpdated", value: finalUpdated)
-                state.lastUpdated = finalUpdated
-                
-                
-                //for some reason the rain and night booleans evaluate as false no matter what, took out if statement for the time being
-                def R =  individualBloomSky.Data.Rain.toString()
-                def rain = R.replaceAll("\\[", "").replaceAll("\\]","")
-                if (rain == "false"){
-                	if (state.rain != "false") {
-                    	sendEvent(name: "rain", value: "Not Raining")
-                        state.rain = "false"
-                    }
-                    if (logging) {
-                			log.debug "Rain:" + rain
-                	} 
-                }
-                else {
-                	if (state.rain != "true") {
-                    	sendEvent(name: "rain", value: "Raining")
-                    	state.rain = "true"
+                def data = [:]
+                data << individualBloomSky.Data //put bloomsky data into hash map
+                //itterate through hashap pairs to update device. Used case statement because it was twice as fast.
+                data.each {oldkey, datum->
+                	def key = oldkey.toLowerCase() //bloomsky returns camel cased keys. Put to lowercase so dth can update correctly
+                	switch(datum) {
+                    	case { key == "voltage"}: //update "battery"
+                        	sendEvent(name:"battery", value:datum)
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "ts"}: //update last update from bloomsky
+                        	def lastUpdateTick = (datum * 1000L) + location.timeZone.rawOffset
+                			def finalUpdated = new java.text.SimpleDateFormat("MMM dd HH:mm").format(lastUpdateTick)
+                			sendEvent(name: "lastUpdated", value: finalUpdated)
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "rain" && datum == false}: //check if it is raining or not
+                        	sendEvent(name: "water", value: "dry")
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "rain" && datum == true}:
+                        	sendEvent(name: "water", value: "wet")
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "luminance"}: //update illuminance
+                        	sendEvent(name: "illuminance", value: datum)
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "uvindex"}: //bloomsky does UV index! how cool is that!?
+                        	sendEvent(name: "ultravioletIndex", value: datum)
+                            log.debug "${key}:${datum}"
+                          break
+                        case { it instanceof Integer && key != "imagets"}: //check for integers. Two data points are integers so we save some expense doing this.
+                        	sendEvent(name:"${key}", value: datum)
+                            log.debug "${key}:${datum}"
+                          break
+                        case { key == "temperature"}: //temperature needs to be converted to celcuis in some cases so we break it out on its own
+                        	def temp = getTemperature(datum).toDouble().trunc(1)
+                        	sendEvent(name:"${key}", value: temp)
+                            log.debug "${key}:${temp}"
+                          break
+                    	case { it instanceof BigDecimal && key != "imagets"}: //rest of the data points are big decimals. Another expense saver.
+                        	def newDatum = datum.toFloat().trunc(1)
+                        	sendEvent(name:"${key}", value: newDatum)
+                            log.debug "${key}:${newDatum}"
+                          break
+                        case { key == "imageurl"}: //lastly update the image from bloomsky to the DTH
+                        	httpGet(datum) { it -> 
+                            	storeImage(getPictureName(), it.data)
+                    		}
+                            log.debug "${key}:${datum}"
+                      	  break
                 	}
-                    if (logging) {
-                			log.debug "Rain:" + rain
-               		} 
-                }    
-                def N =  individualBloomSky.Data.Night.toString()
-                def night = N.replaceAll("\\[", "").replaceAll("\\]","")
-                if (night == "false") {
-                	if (state.night != "false") {
-                    	sendEvent(name: "day", value: "It's day time")
-                    	state.night = "false"
-                    }
-                    if (logging) {
-              			log.debug "Night:" + night
-              		}
-                }
-                else {
-                	if (state.night != "true") {
-                    	sendEvent(name: "day", value: "It's night time")
-                        state.night = "true"
-                        if (logging) {
-                			log.debug "Night:" + night
-                		} 
-                    }
-                    if (logging) {
-              			log.debug "Night:" + night
-              		}
-                }
-        		 
-        }
+            	}
+        	}
         }catch (Exception e) { //log exception gracefully
 			log.debug "Error: $e"
-		}   
-        
-        
-
+		}  
 }
 
 //create unique name for picture storage
